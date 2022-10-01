bf0ba5292239761fc48fadf667e219343e0c5a4e
Amit Kumar Singh <amit.aanmol@gmail.com>
2022-10-02 02:59:05 +0530

    Intial project setup

---
+++ AASwiftUtils/AASwiftUtils.h
@@ -0,0 +1,18 @@
+//
+//  AASwiftUtils.h
+//  AASwiftUtils
+//
+//  Created by Amit Kumar Singh on 02/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AASwiftUtils.
+FOUNDATION_EXPORT double AASwiftUtilsVersionNumber;
+
+//! Project version string for AASwiftUtils.
+FOUNDATION_EXPORT const unsigned char AASwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AASwiftUtils/PublicHeader.h>
+
+
---
+++ AASwiftUtilsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AASwiftUtilsExample
+//
+//  Created by Amit Kumar Singh on 02/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
